657af1f5ea2cd0c62642313b229819db7d7f0625
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2012-04-26 22:44:33 -0300

    Selection sort added

--- makefile
+++ makefile
@@ -7,5 +7,5 @@ HDR = headers/
 $(SRC)*.c : $(HDR)*.h
 	gcc -c $(SRC)bubble_sort.c -o $(OBJ)bubble_sort.o
 	gcc -c $(SRC)insertion_sort.c -o $(OBJ)insertion_sort.o
-
+	gcc -c $(SRC)selection_sort.c -o $(OBJ)selection_sort.o
 	
---
+++ source/selection_sort.c
@@ -0,0 +1,20 @@
+#ifndef __SELECTIONSORT
+#define __SELECTIONSORT
+
+void selectionSort(int* L, int tam){
+    int min, bck, i, j;
+    for(i=0;i<tam-1;i++){
+        min = i;
+        for(j=i+1;j<tam;j++){
+            if(L[j]<L[min]) min = j;
+        }
+        //Swap--------
+        bck = L[i];
+        L[i] = L[min];
+        L[min] = bck;
+        //------------
+    }
+    return;
+}
+
+#endif
