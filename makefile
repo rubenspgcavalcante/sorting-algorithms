ea923eb86efc0c38587d4a425b3a7edaee264d72
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2012-04-26 21:53:18 -0300

    Bubble-sort and makefile added

---
+++ makefile
@@ -0,0 +1,4 @@
+# -- Rules -- #
+objects/bubble_sort.o : source/bubble_sort.c
+	gcc -c source/bubble_sort.c -o objects/bubble_sort.o
+
---
+++ source/bubble_sort.c
@@ -0,0 +1,26 @@
+#ifndef __BUBBLESORT
+#define __BUBBLESORT
+
+void bubbleSort(int* L, int tam){
+    int i, j;
+    int bck;
+    int flag;
+    i=1;
+    do{
+        flag = 0;
+        for(j=0;j<tam-i;j++){
+            if(L[j]>L[j+1]){
+                //Swap-------------
+                    bck = L[j];
+                    L[j] = L[j+1];
+                    L[j+1] = bck;
+                //-----------------
+                flag = 1;
+            }
+        }
+        i++;
+    }while(flag);
+    return;
+}
+
+#endif
